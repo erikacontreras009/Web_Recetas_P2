5c76a844352ac1837f6d5315c8c20478fc112624
erikacontreras009 <139599016+erikacontreras009@users.noreply.github.com>
2023-10-17 23:55:30 -0500

    Pasos
    123

--- RecetasWEB2/js/index.js
+++ RecetasWEB2/js/index.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Obtener el formulario y los campos de entrada
     const form = document.getElementById("survey-form");
-    const nombreInput = document.getElementById("nombre");
-    const edadInput = document.getElementById("edad");
-    const opinionInput = document.getElementById("opinion");
-    const enlaceInput = document.getElementById("enlace");
+    const nombreInput = document.getElementById("Nom_Receta");
+    const IngredientesInput = document.getElementById("Ingredientes");
+    const PasosInput = document.getElementById("Pasos");
+    
   
     // Agregar un controlador de eventos para enviar el formulario
     form.addEventListener("submit", function(event) {
@@ -12,25 +12,25 @@ document.addEventListener("DOMContentLoaded", function() {
   
       // Obtener los valores de los campos de entrada
       const nombre = nombreInput.value;
-      const edad = edadInput.value;
-      const opinion = opinionInput.value;
-      const enlace = enlaceInput.value;
-  
+      const Ingredientes = IngredientesInput.value;
+      const Pasos = PasosInput.value;
+      
+
       // Validar que los campos no estén vacíos
-      if (nombre.trim() === "" || edad.trim() === "" || opinion.trim() === "" || enlace.trim() === "") {
+      if (nombre.trim() === "" || Ingredientes.trim() === "" || Pasos.trim() === "" ) {
         alert("Por favor, completa todos los campos.");
         return;
       }
   
       // Guardar los datos en el Local Storage
-      saveDataToLocalStorage(nombre, edad, opinion, enlace);
+      saveDataToLocalStorage(nombre, Ingredientes, Pasos);
   
       // Reiniciar los campos del formulario
       form.reset();
     });
   
     // Función para guardar los datos en el Local Storage
-    function saveDataToLocalStorage(nombre, edad, opinion, enlace) {
+    function saveDataToLocalStorage(nombre, Ingredientes, Pasos) {
       // Obtener los datos anteriores del Local Storage
       let storedData = localStorage.getItem("surveyData");
       if (storedData === null) {
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function() {
       }
   
       // Agregar los nuevos datos a la lista
-      storedData.push({ nombre, edad, opinion, enlace });
+      storedData.push({ nombre, Ingredientes, Pasos});
   
       // Guardar la lista actualizada en el Local Storage
       localStorage.setItem("surveyData", JSON.stringify(storedData));
